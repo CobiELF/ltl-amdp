78432c858ccbc9ceb86bc8ee1555ebcbe7391b82
Cobi Finkelstein <cobi.finkelstein@gmail.com>
2019-06-11 19:29:32 -0400

    first shot at compiling usable alloy code. i cheated a lot

--- mod/compiler.py
+++ mod/compiler.py
@@ -1,10 +1,10 @@
 #imports
 import sys
-from lark import Lark, tree
+from lark import Lark, tree, Visitor, Transformer
 
 ltl_grammar = """
     ltl : next
-        | finally
+        | future
         | global
         | until
         | "(" ltl ")"
@@ -13,7 +13,7 @@ ltl_grammar = """
     
     next: "X" ltl
 
-    finally: "F" ltl
+    future: "F" ltl
 
     global: "G" ltl
 
@@ -34,17 +34,67 @@ ltl_grammar = """
         %ignore WS
 """
 
+ltl_grammar_ez = """
+    ltl : next
+        | future
+        | global
+        | until
+        | "(" ltl ")"
+        | state
+        | TERM
+    
+    next: "X" TERM
+
+    future: "F" TERM
+
+    global: "G" TERM
+
+    until: TERM "U" TERM
+
+    state : neg
+          | con
+    
+    neg: "~" TERM
+
+    con: TERM "&" TERM
+
+    TERM : "red_room" | "orange_room" | "yellow_room" | "green_room" | "blue_room" | "purple_room"
+            | "landmark_1" | "landmark_2" | "landmark_3" | "landmark_4" | "landmark_5"
+            | "first_floor" | "second_floor" | "third_floor" | "fourth_floor" | "fifth_floor"
+
+        %import common.WS
+        %ignore WS
+"""   
+
+class LarkTransformer(Transformer):
+    def until(self, ptree):
+        print("""
+    some u:Time | {
+        all t:u^prev-u | Robot.where[t] in %s.at
+        Robot.where[u] in %s.at
+    }""" % (ptree[0], ptree[1]))
+
+    def term(self, ptree):
+        print("in %s.at" % ptree[0])
+
+    def neg(self, ptree):
+        pass
+
 def make_ltl_ast(grounding):
     """
     make_ltl_ast(grounding: str) -> Tree
     parse an ltl ast from a given grounding string
     """
 
-    parser = Lark(ltl_grammar, start='ltl', ambiguity='explicit')
-    tree = parser.parse(grounding)
-    print(tree.pretty())
-    for subtree in tree.iter_subtrees():
-        print(subtree.children)
+    parser = Lark(ltl_grammar_ez, start='ltl', ambiguity='explicit')
+    ptree = parser.parse(grounding)
+    
+    return ptree
+
+def compile(ptree, name="grounding"):
+    print("pred %s {" % (name))
+    LarkTransformer().transform(ptree)
+    print("}")
 
 def tests():
     """
@@ -57,7 +107,10 @@ def tests():
     6) ~ green_room & green_room
     """
 
-    make_ltl_ast("(~red_room) U green_room") # 1
+    goal0 = make_ltl_ast("red_room U green_room")
+    print(goal0)
+    compile(goal0)
+    #goal1 = make_ltl_ast("(~red_room) U green_room") # 1
 
 def make_png(grounding):
     parser = Lark(ltl_grammar, start='ltl', ambiguity='explicit')
@@ -65,4 +118,4 @@ def make_png(grounding):
 
 if __name__ == "__main__":
     tests()
-    make_png('(~red_room) U green_room')
+    #make_png('(~red_room) U green_room')
