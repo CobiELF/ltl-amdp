dbb976e6b155e309dcc1d74b8a1992c97a28dbe7
Cobi Finkelstein <cobi.finkelstein@gmail.com>
2019-06-12 12:12:07 -0400

    updated grammar

--- mod/compiler.py
+++ mod/compiler.py
@@ -9,22 +9,24 @@ ltl_grammar = """
         | until
         | "(" ltl ")"
         | state
-        | TERM
+        | term
     
-    next: "X" ltl
+    next : "X" ltl
 
-    future: "F" ltl
+    future : "F" ltl
 
-    global: "G" ltl
+    global : "G" ltl
 
-    until: ltl "U" ltl
+    until : ltl "U" ltl
 
     state : neg
           | con
     
-    neg: "~" ltl
+    neg : "~" ltl
 
-    con: ltl "&" ltl
+    con : ltl "&" ltl
+
+    term : TERM
 
     TERM : "red_room" | "orange_room" | "yellow_room" | "green_room" | "blue_room" | "purple_room"
             | "landmark_1" | "landmark_2" | "landmark_3" | "landmark_4" | "landmark_5"
@@ -33,52 +35,6 @@ ltl_grammar = """
         %import common.WS
         %ignore WS
 """
-
-ltl_grammar_ez = """
-    ltl : next
-        | future
-        | global
-        | until
-        | "(" ltl ")"
-        | state
-        | TERM
-    
-    next: "X" TERM
-
-    future: "F" TERM
-
-    global: "G" TERM
-
-    until: TERM "U" TERM
-
-    state : neg
-          | con
-    
-    neg: "~" TERM
-
-    con: TERM "&" TERM
-
-    TERM : "red_room" | "orange_room" | "yellow_room" | "green_room" | "blue_room" | "purple_room"
-            | "landmark_1" | "landmark_2" | "landmark_3" | "landmark_4" | "landmark_5"
-            | "first_floor" | "second_floor" | "third_floor" | "fourth_floor" | "fifth_floor"
-
-        %import common.WS
-        %ignore WS
-"""   
-
-class LarkTransformer(Transformer):
-    def until(self, ptree):
-        print("""
-    some u:Time | {
-        all t:u^prev-u | Robot.where[t] in %s.at
-        Robot.where[u] in %s.at
-    }""" % (ptree[0], ptree[1]))
-
-    def term(self, ptree):
-        print("in %s.at" % ptree[0])
-
-    def neg(self, ptree):
-        pass
 
 def make_ltl_ast(grounding):
     """
@@ -90,11 +46,6 @@ def make_ltl_ast(grounding):
     ptree = parser.parse(grounding)
     
     return ptree
-
-def compile(ptree, name="grounding"):
-    print("pred %s {" % (name))
-    LarkTransformer().transform(ptree)
-    print("}")
 
 def tests():
     """
@@ -108,12 +59,11 @@ def tests():
     """
 
     goal0 = make_ltl_ast("red_room U green_room")
-    print(goal0)
-    compile(goal0)
+    print(goal0.pretty())
     #goal1 = make_ltl_ast("(~red_room) U green_room") # 1
 
 def make_png(grounding):
-    parser = Lark(ltl_grammar, start='ltl', ambiguity='explicit')
+    parser = Lark(ltl_grammar_ez, start='ltl', ambiguity='explicit')
     tree.pydot__tree_to_png(parser.parse(grounding), './parse.png')
 
 if __name__ == "__main__":
