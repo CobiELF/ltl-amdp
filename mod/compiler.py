395742cf9f480f5123f18781f9864047eedbca8e
Cobi Finkelstein <cobif@protonmail.com>
2019-06-12 16:44:37 -0400

    first steps to a compiler with less cheating. i found out how transformers work

--- mod/compiler.py
+++ mod/compiler.py
@@ -36,16 +36,53 @@ ltl_grammar = """
         %ignore WS
 """
 
+class LTLTransformer(Transformer):
+    #TODO handle "futures" modifying "untils"
+    def ltl(self, children):
+        return children[0]
+
+    def state (self, children):
+        return children[0]
+
+    def neg(self, children):
+        return "not " + children[0]
+    
+    def until(self, children):
+        return """
+        some u:Time | {
+            all t:u.^prev-u | Robot.where[t] %s
+            Robot.where[u] %s
+        }""" % (children[0], children[1])
+
+    def con(self, children):
+        return """
+        Robot.where[first] %s
+        Robot.where[first] %s
+        """ % (children[0], children[1])
+
+    def future(self, children): #TODO
+        pass
+
+    def term(self, children):
+        return "in %s.at" % children[0]
+
 def make_ltl_ast(grounding):
     """
     make_ltl_ast(grounding: str) -> Tree
     parse an ltl ast from a given grounding string
     """
 
-    parser = Lark(ltl_grammar_ez, start='ltl', ambiguity='explicit')
+    parser = Lark(ltl_grammar, start='ltl', ambiguity='explicit')
     ptree = parser.parse(grounding)
     
     return ptree
+
+def compile_tree(ptree, name="grounding"):
+    #TODO maybe make this take i na grounding then just do the whole thing?
+
+    print("pred %s {" % name)
+    print(LTLTransformer().transform(ptree))
+    print("}")
 
 def tests():
     """
@@ -53,17 +90,22 @@ def tests():
     1) ~ red_room U green_room
     2) F red_room U green_room
     3) F green_room U green_room
-    4) ~ red_room U green_room
+    4) ~ red_room & green_room
     5) F red_room & green_room
     6) ~ green_room & green_room
     """
 
-    goal0 = make_ltl_ast("red_room U green_room")
-    print(goal0.pretty())
-    #goal1 = make_ltl_ast("(~red_room) U green_room") # 1
+    goal1 = make_ltl_ast("(~red_room) U green_room")
+    goal2 = make_ltl_ast("F (red_room U green_room)")
+    goal3 = make_ltl_ast("F (green_room U green_room)")
+    goal4 = make_ltl_ast("(~ red_room) & green_room")
+    goal5 = make_ltl_ast("F (red_room & green_room)")
+    goal6 = make_ltl_ast("(~ green_room) & green_room")
 
+    #TODO stuff with goals
+    
 def make_png(grounding):
-    parser = Lark(ltl_grammar_ez, start='ltl', ambiguity='explicit')
+    parser = Lark(ltl_grammar, start='ltl', ambiguity='explicit')
     tree.pydot__tree_to_png(parser.parse(grounding), './parse.png')
 
 if __name__ == "__main__":
