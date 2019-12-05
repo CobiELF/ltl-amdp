43673abe882d1640a1332f140c03e7311c735082
Cobi Finkelstein <cefinkelstei@umass.edu>
2019-12-05 16:48:03 -0500

    fixed datasets

--- mod/make_tsvs.py
+++ mod/make_tsvs.py
@@ -1,29 +1,20 @@
 if __name__ == "__main__":
     # create test tsvs
     for file_number in range(1,6):
-        fp_src = open("mod/TEST_" + str(file_number) + "_SRC", 'r')
-        fp_tar = open("mod/TEST_" + str(file_number) + "_TAR", 'r')
-        fp_tsv = open("mod/TEST_" + str(file_number) + ".tsv", 'w')
+        fp_train_src = open("TRAIN_" + str(file_number) + "_SRC", 'r')
+        fp_train_tar = open("TRAIN_" + str(file_number) + "_TAR", 'r')
+        fp_test_src = open("TEST_" + str(file_number) + "_SRC", 'r')
+        fp_test_tar = open("TEST_" + str(file_number) + "_TAR", 'r')
 
-        for line in fp_src:
-            fp_tsv.write(line[:-1] + "\t" + fp_tar.readline())
+        fp_train_tsv = open("TRAIN_" + str(file_number) + ".tsv", 'w')
+        fp_test_tsv = open("TEST_" + str(file_number) + ".tsv", 'w')
+        for line in fp_train_src:
+            fp_train_tsv.write(line[:-1] + "\t" + fp_train_tar.readline())
+            fp_test_tsv.write(fp_test_src.readline()[:-1] + "\t" + fp_test_tar.readline())
 
-        fp_src.close()
-        fp_tar.close()
-        fp_tsv.close()
-
-    for holdout in range(1,6):
-        print(holdout)
-        fp_train = open("mod/TRAIN_" + str(holdout) + ".tsv", 'w')
-        for test_num in range(1, holdout):
-            fp_test = open("mod/TEST_" + str(file_number) + ".tsv", 'r')
-            for line in fp_test:
-                fp_train.write(line)
-            fp_test.close()
-        for test_num in range(holdout+1, 6):
-            fp_test = open("mod/TEST_" + str(file_number) + ".tsv", 'r')
-            for line in fp_test:
-                fp_train.write(line)
-            fp_test.close()
-        fp_train.close()
-        
+        fp_train_src.close()
+        fp_train_tar.close()
+        fp_test_src.close()
+        fp_test_tar.close()
+        fp_train_tsv.close()
+        fp_test_tsv.close()
