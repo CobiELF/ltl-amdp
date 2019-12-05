b25557d9fc6955dbe7223bb121c5f07d2c56d75a
Cobi Finkelstein <cefinkelstei@umass.edu>
2019-12-05 18:15:58 -0500

    hopefully finally solved all dataset issues

--- mod/make_tsvs.py
+++ mod/make_tsvs.py
@@ -1,20 +1,31 @@
 if __name__ == "__main__":
     # create test tsvs
     for file_number in range(1,6):
+        #training dataset
         fp_train_src = open("TRAIN_" + str(file_number) + "_SRC", 'r')
         fp_train_tar = open("TRAIN_" + str(file_number) + "_TAR", 'r')
-        fp_test_src = open("TEST_" + str(file_number) + "_SRC", 'r')
-        fp_test_tar = open("TEST_" + str(file_number) + "_TAR", 'r')
-
         fp_train_tsv = open("TRAIN_" + str(file_number) + ".tsv", 'w')
-        fp_test_tsv = open("TEST_" + str(file_number) + ".tsv", 'w')
-        for line in fp_train_src:
-            fp_train_tsv.write(line[:-1] + "\t" + fp_train_tar.readline())
-            fp_test_tsv.write(fp_test_src.readline()[:-1] + "\t" + fp_test_tar.readline())
+
+        src_lines = fp_train_src.readlines()
+        tar_lines = fp_train_tar.readlines()
+        for i in range(len(src_lines)):
+            fp_train_tsv.write(src_lines[i][:-1] + "\t" + tar_lines[i])
 
         fp_train_src.close()
         fp_train_tar.close()
+        fp_train_tsv.close()
+
+        # test dataset
+
+        fp_test_src = open("TEST_" + str(file_number) + "_SRC", 'r')
+        fp_test_tar = open("TEST_" + str(file_number) + "_TAR", 'r')
+        fp_test_tsv = open("TEST_" + str(file_number) + ".tsv", 'w')
+
+        src_lines = fp_test_src.readlines()
+        tar_lines = fp_test_tar.readlines()
+        for i in range(len(src_lines)):
+            fp_test_tsv.write(src_lines[i][:-1] + "\t" + tar_lines[i])
+
         fp_test_src.close()
         fp_test_tar.close()
-        fp_train_tsv.close()
         fp_test_tsv.close()
